af17dc42aa9a0219deeb12829728b60f43ea94c3
shoumoji <kosyu79@gmail.com>
2020-11-14 17:00:27 +0900

    2つのjsonを縦につなげてurl作りたい

--- search.sh
+++ search.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-for ((page=1; page<=10; page++))
-do
-    curl -H \
-        'Accept: application/vnd.github.preview.text-match+json' \
-        "https://api.github.com/search/repositories?q=page:${page}&language:javascript&filename=package.json&order=desc" \
-        >> data.json
-done
+# 毎回動かすと怒られそうなので一旦コメントアウト
+#for ((page=1; page<=10; page++))
+#do
+#    curl -H \
+#        'Accept: application/vnd.github.preview.text-match+json' \
+#        "https://api.github.com/search/repositories?q=page:${page}&language:javascript&filename=package.json&order=desc" \
+#        >> data.json
+#done
 
 jq .items[].html_url < data.json > repos.json
+jq .items[].default_branch < data.json > branch.json
+
+# 2つのjsonをうまくがちゃっとくっつけていい感じのarchive.json作りたさある
+
