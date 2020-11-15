3f780d72d546bb7131879b06c01cf484d013d7cc
shoumoji <kosyu79@gmail.com>
2020-11-16 00:42:54 +0900

    todo: DLしたファイルの名前をつける

--- search.sh
+++ search.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
+set -eu
 
-# 毎回動かすと怒られそうなので一旦コメントアウト
-#for ((page=1; page<=10; page++))
-#do
-#    curl -H \
-#        'Accept: application/vnd.github.preview.text-match+json' \
-#        "https://api.github.com/search/repositories?q=page:${page}&language:javascript&filename=package.json&order=desc" \
-#        >> data.json
-#done
+if [ -e data.json ]; then
+    rm data.json
+fi
+
+for ((page=1; page<=3; page++))
+do
+    curl -H \
+        'Accept: application/vnd.github.preview.text-match+json' \
+        "https://api.github.com/search/repositories?q=page:${page}&language:javascript&filename=package.json&order=desc" \
+        >> data.json
+done
 
 jq .items[].html_url < data.json > repos.json
-jq .items[].default_branch < data.json > branch.json
+jq .items[].default_branch < data.json > branchs.json
 
-# 2つのデータのダブルクォート外す
+paste repos.json branchs.json > raw_archive_url
+sed -r 's/^\"(.*?)\".*?\"(.*?)\"$/\1\/archive\/\2.zip/g' < raw_archive_url > archive_url
 
-# 2つのjsonをうまくがちゃっとくっつけていい感じのarchive_url作りたさある
-paste repos.json branch.json > raw_archive_url
+rm repos.json branchs.json raw_archive_url data.json
+
+# change directory -> projects/
+if [ -d projects ]; then
+    cd projects
+    rm -rf *
+else
+    mkdir projects
+    cd projects
+fi
+
+while read line
+do
+    # todo: ファイル名いい感じにする
+    FILENAME=
+    curl -O $line -o $NAME
+done < ../archive_url
+
+unzip *
